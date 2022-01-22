f369cffd8af085f0b73f7edb94863a827c83383e
Roy <royissa3@gmail.com>
2022-01-22 18:50:21 +0200

    get recipe btn

--- script.js
+++ script.js
@@ -1,48 +1,86 @@
+// variables
+
 const search = document.querySelector("input");
 const btn = document.querySelector("#btn");
 const container = document.querySelector(".container");
 const result = document.querySelector(".result");
-// const recipe = document.querySelectorAll("button.recipe");
-// const recipeCard = document.querySelectorAll("recipe-card");
 
+// invoke on load
+getdata();
+// invoke when click search
 btn.addEventListener("click", getdata);
 
-function getdata() {
-  let ing = search.value.trim();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function get data search click
+
+function getdata(arg = "ham") {
+  let ing;
+  search.value.trim() !== "" ? (ing = search.value.trim()) : (ing = arg);
+  console.log(ing);
   let html = "";
   fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ing}`)
     .then((res) => res.json())
     .then((data) => {
       data.meals.forEach((meal) => {
         html += `
-          <div class="card">
+          <div class="card" data-id="${meal.idMeal}">
                   <img src="${meal.strMealThumb}" alt="food">
                   <p>${meal.strMeal}</p>
-                  <button class="recipe" id="recipe">Get Recipe</button>
-                <div class="recipe-card hiden">
+                  <button class="recipe">Get Recipe</button>
+              <div class="recipe-card hiden">
                   <button class="close"><i class="fas fa-times-circle"></i></button>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat neque laudantium, non sunt et optio. Mollitia porro culpa aperiam eius fugiat quis, nam assumenda veritatis suscipit illo vero accusamus id!</p>
+                  <p></p>
                 </div>
-              
-              </div>      
-
-         
+              </div>
         `;
-
-        console.log(meal);
       });
-      result.classList.remove("hiden");
       container.innerHTML = html;
+    })
+    .then(() => {
+      const recipeBtn = document.querySelectorAll(".recipe");
+      recipeBtn.forEach((e) => {
+        e.addEventListener("click", function (e) {
+          let show = e.target.nextElementSibling;
+          let thisCard = e.target.parentNode;
+          let hide = e.target.nextElementSibling.querySelector("button");
+          console.log(thisCard.getAttribute("data-id"));
+          getrecipebtn(thisCard);
+          show.classList.remove("hiden");
+          hide.addEventListener("click", function (e) {
+            show.classList.add("hiden");
+          });
+        });
+      });
     });
 }
 
-const recipeBtn = document.getElementById("recipe");
-const recipeCard = document.querySelector(".recipe-card");
-const btnClose = document.querySelector(".close");
 
-btnClose.addEventListener("click", () => {
-  recipeCard.classList.add("hiden");
-});
-recipeBtn.addEventListener("click", () => {
-  recipeCard.classList.remove("hiden");
-});
+
+
+// function get data for the hidden element
+
+async function getrecipebtn(id) {
+  // console.log(typeof mealRecipe.idMeal);
+  await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id.getAttribute(
+    "data-id"
+  )}
+  `)
+    .then((res) => res.json())
+    .then(
+      (dt) =>
+        (id.querySelector(".recipe-card").querySelector("p").innerText =
+          dt.meals[0].strInstructions)
+    );
+}
