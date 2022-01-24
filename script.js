d162b0925449b6939c29dfb3775651653a78d35c
Roy <royissa3@gmail.com>
2022-01-24 04:48:37 +0200

    learned event bubbling n propagation

--- script.js
+++ script.js
@@ -11,30 +11,18 @@ getdata();
 btn.addEventListener("click", getdata);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// function get data search click
+// fetch search click
 
 function getdata(arg = "ham") {
-  let ing;
-  search.value.trim() !== "" ? (ing = search.value.trim()) : (ing = arg);
-  console.log(ing);
-  let html = "";
-  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ing}`)
-    .then((res) => res.json())
-    .then((data) => {
-      data.meals.forEach((meal) => {
-        html += `
+    let ing;
+    search.value.trim() !== "" ? (ing = search.value.trim()) : (ing = arg);
+    console.log(ing);
+    let html = "";
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ing}`)
+        .then((res) => res.json())
+        .then((data) => {
+            data.meals.forEach((meal) => {
+                html += `
           <div class="card" data-id="${meal.idMeal}">
                   <img src="${meal.strMealThumb}" alt="food">
                   <p>${meal.strMeal}</p>
@@ -45,42 +33,31 @@ function getdata(arg = "ham") {
                 </div>
               </div>
         `;
-      });
-      container.innerHTML = html;
-    })
-    .then(() => {
-      const recipeBtn = document.querySelectorAll(".recipe");
-      recipeBtn.forEach((e) => {
-        e.addEventListener("click", function (e) {
-          let show = e.target.nextElementSibling;
-          let thisCard = e.target.parentNode;
-          let hide = e.target.nextElementSibling.querySelector("button");
-          console.log(thisCard.getAttribute("data-id"));
-          getrecipebtn(thisCard);
-          show.classList.remove("hiden");
-          hide.addEventListener("click", function (e) {
+            });
+            container.innerHTML = html;
+        })
+
+}
+
+// get recipe fetch
+container.addEventListener("click", function(e) {
+    if (e.target.classList.contains("recipe")) {
+        let show = e.target.nextElementSibling,
+            hide = e.target.nextElementSibling.querySelector("button"),
+            id = e.target.parentNode;
+        show.classList.remove("hiden");
+        hide.addEventListener("click", function(e) {
             show.classList.add("hiden");
-          });
-        });
-      });
-    });
-}
-
-
-
-
-// function get data for the hidden element
-
-async function getrecipebtn(id) {
-  // console.log(typeof mealRecipe.idMeal);
-  await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id.getAttribute(
-    "data-id"
-  )}
+        })
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id.getAttribute(
+      "data-id"
+    )}
   `)
-    .then((res) => res.json())
-    .then(
-      (dt) =>
-        (id.querySelector(".recipe-card").querySelector("p").innerText =
-          dt.meals[0].strInstructions)
-    );
-}
+            .then((res) => res.json())
+            .then((dt) => {
+                id.querySelector(".recipe-card").querySelector("p").innerHTML =
+                    dt.meals[0].strInstructions;
+            });
+    }
+
+})
